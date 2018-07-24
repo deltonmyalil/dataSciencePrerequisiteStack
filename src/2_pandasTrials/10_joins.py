ff865ab0463e3f0df6117d1b7fa2aa397554a27d
deltonmyalil <deltonguns@gmail.com>
2018-07-24 19:40:46 +0530

    Join two tables. Also, pandas relevant topics completed.

--- src/2_pandasTrials/10_joins.py
+++ src/2_pandasTrials/10_joins.py
@@ -3,3 +3,19 @@ import pandas as pd
 # This uses table1.csv and table2.csv
 
 t1 = pd.read_csv("table1.csv")
+t2 = pd.read_csv("table2.csv")
+print("Printing both tables separately")
+print("Table 1")
+print(t1)
+print()
+print("Table 2")
+print(t2)
+print()
+
+# Let us join the two tables with respect to user_id attribute. Like natural join.
+m = pd.merge(t1, t2, on="user_id")  # join operation of t1 and t2 on the attribute user_id,
+# you can also join on more than one attribute
+# you can also do t1.merge(t2, on="user_id")
+print()
+print("Printing joined table")
+print(m)
