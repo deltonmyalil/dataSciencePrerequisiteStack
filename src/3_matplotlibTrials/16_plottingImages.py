957a45fc30892a1c193fe01281f4616d19251b75
deltonmyalil <deltonguns@gmail.com>
2018-07-26 23:09:15 +0530

    plotting an image

--- src/3_matplotlibTrials/16_plottingImages.py
+++ src/3_matplotlibTrials/16_plottingImages.py
@@ -7,4 +7,19 @@ df = pd.read_csv("train.csv")  # file has a header
 print("read the csv file, and the shape is {0} and size is {1}".format(df.shape, df.size))
 
 M = df.values
+image = M[0, 1:]  # Select the 0th row and take every column except the first as it is not a pixel, but a header
+# I think it is M[column, row] ie the other way round, need to confirm
 
+print("Shape of image is ", image.shape)  # It is a vector of size 728 in single row
+
+image = image.reshape(28, 28)
+
+print("After reshaping, the image is of shape ", image.shape)
+
+# Now plot the image
+plt.imshow(image)  # Optional parameter cmap="gray" to view the image in grayscale
+plt.show()
+
+# if you do
+# plt.imshow(255-image, cmap='gray')
+# then the grayscale image color will be inverted
